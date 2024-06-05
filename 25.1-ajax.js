3ba93b77f95ea532b4080023036c06e0e507d62c
ErickQuecano <1533971@senati.pe>
2024-06-05 17:44:56 -0500

    conectar con ajax y formulario

--- 25.1-ajax.js
+++ 25.1-ajax.js
@@ -12,9 +12,33 @@ let datos ={
     type :"post",
     data : datos,
     success : function(result){
+        debugger;
         console.log(result);
+        const pacientes=$.parseJSON(result);
+        pacientes.forEach(item =>{
+            agregarFilas("#tabla",item);});
     } 
 
     })
     return;
+}
+
+function agregarFilas(id,paciente) {
+    const html=
+    "<tr>"+
+    "<td>"+paciente.nombres+"</td>"+
+    "<td>"+paciente.edad+"</td>"+    
+    "<td>"+paciente.talla_m+"</td>"+
+    "<td>"+paciente.peso_kg+"</td>"+
+    "<td>"+paciente.sintoma_tos+"</td>"+
+    "<td>"+paciente.sintoma_fiebre+"</td>"+
+    "<td>"+paciente.sintoma_disnea+"</td>"+
+    "<td><button type='button' onclick=editar('"+paciente.nombres+"','"+paciente.edad+"');>Editar</button></td>"+
+    "</tr>";
+    $(id+" tr:last").after(html);
+}
+ 
+function editar(nombres,edad) {
+    $('#exampleModal').modal('show');    
+    $("#nombre2").val(nombres);
 }
